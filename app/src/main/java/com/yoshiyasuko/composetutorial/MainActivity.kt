6901e05889edc1c8e9ed593dd5a3d3d176ce53c3
Yoshiyasu KO <te108128@gmail.com>
2023-02-08 23:08:02 +0900

    show Hello World in Preview

--- app/src/main/java/com/yoshiyasuko/composetutorial/MainActivity.kt
+++ app/src/main/java/com/yoshiyasuko/composetutorial/MainActivity.kt
@@ -16,28 +16,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(name = "Android")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(name: String) {
+    Text(text = "Hello $name")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
 }
